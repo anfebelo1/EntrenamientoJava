eace7fc03154a04cc7b1d4b118d7ccd6159ec076
andres.bermudez <abermudez@intergrupo.com>
2025-04-08 17:06:02 -0500

    first commit 04-08-2025 carga del proyecto

---
+++ src/com/empresa/TiposDeVariablesSegunDato.java
@@ -0,0 +1,22 @@
+package com.empresa;
+
+public class TiposDeVariablesSegunDato {
+	
+	
+
+	public static void main(String[] args) {
+		byte edad = 10;
+		short  numero=32000;
+		int contador = 1000;
+		long distancia = 100000L;
+		float precio= 9.99f;
+		double salario=12345.67;
+		char letra='A';
+		boolean activo=true;
+		String nombre="Pruebas";
+		
+		System.out.println("byte edad=" + edad );
+ 
+	}
+
+}
---
+++ src/com/empresa/VariableStatica.java
@@ -0,0 +1,13 @@
+package com.empresa;
+
+public class VariableStatica {
+	
+	static int MAX_USUARIOS=100;
+	
+
+	public static void main(String[] args) {
+
+		System.out.println("Maximo de usuarios:" + VariableStatica.MAX_USUARIOS);
+	}
+
+}
